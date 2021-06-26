8a89f784e00aaa4b10d27859d162aa7d6f090bf4
Suhas <suhasdev56@gmail.com>
2021-06-26 14:48:57 +0000

    cleanup

--- src/Database/Location.php
+++ src/Database/Location.php
@@ -4,19 +4,16 @@ namespace App\Database;
 use PDO;
 class Location extends Database {
 
-public function add_location($woeid, $locations, $email){
-    $query = $this->db->prepare('SELECT * from users WHERE email_address = :email_address');
-    $query -> execute([
-        ':email_address' => $email]);
-    $location = $query->fetch(PDO::FETCH_ASSOC);
-    $location_query = $this->db->prepare('INSERT into location (woeid, location, user_id) VALUES (:woeid, :location, :user_id)');
-    $location_query->execute([
-      ':woeid' => $woeid,
-      ':location' => $locations,
-      ':user_id' => $location['id'],]);
-
-}
-
-
-
+    public function add_location($woeid, $locations, $email){
+        $query = $this->db->prepare('SELECT * from users WHERE email_address = :email_address');
+        $query -> execute([
+            ':email_address' => $email]);
+        $user = $query->fetch(PDO::FETCH_ASSOC);
+        $location_query = $this->db->prepare('INSERT into location (woeid, location, user_id) VALUES (:woeid, :location, :user_id)');
+        $location_query->execute([
+        ':woeid' => $woeid,
+        ':location' => $locations,
+        ':user_id' => $user['id'],
+        ]);
+    }
 }
